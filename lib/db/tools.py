d63005b52a9545ad91f554d3546364010778fea4
crea-tivecoder <nyawirawaweru.com>
2024-09-24 13:15:01 +0300

    modify cli.py

--- lib/db/tools.py
+++ lib/db/tools.py
@@ -1,8 +1,8 @@
-# Import the necessary modules from the SQLAlchemy library
+#Import the necessary modules from the SQLAlchemy library
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# Create an engine that connects to the SQLite database
+#Create an engine that connects to the SQLite database
 engine = create_engine('sqlite:///inventories.db')
 
 #Create a configured "Session" class
